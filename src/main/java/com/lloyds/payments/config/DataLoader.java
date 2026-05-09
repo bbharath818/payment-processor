a934eb9efcb03bdcbb1bbbca6c635ba01b377f38
sunnymehta1997 <sunnymehta1987@gmail.com>
2026-05-09 15:26:07 +0530

    Data Loading payment

--- src/main/java/com/lloyds/payments/config/DataLoader.java
+++ src/main/java/com/lloyds/payments/config/DataLoader.java
@@ -3,7 +3,9 @@ package com.lloyds.payments.config;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lloyds.payments.entity.Account;
+import com.lloyds.payments.entity.PaymentOutcome;
 import com.lloyds.payments.repository.AccountRepository;
+import com.lloyds.payments.repository.PaymentOutcomeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.io.ClassPathResource;
@@ -11,34 +13,73 @@ import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.time.Instant;
 
 @Component
 @RequiredArgsConstructor
 public class DataLoader implements CommandLineRunner {
 
     private final AccountRepository accountRepository;
+    private final PaymentOutcomeRepository paymentRepository;
     private final ObjectMapper objectMapper;
 
     @Override
     public void run(String... args) throws Exception {
+        loadAccounts();
+        loadPayments();
+    }
 
-        // avoid duplicate insert
-        if (accountRepository.count() > 0) {
-            return;
-        }
-
-        InputStream inputStream =
-                new ClassPathResource("accounts.json").getInputStream();
+    private void loadAccounts() throws Exception {
+        if (accountRepository.count() > 0) return;
 
         List<Account> accounts =
-                objectMapper.readValue(
-                        inputStream,
-                        new TypeReference<List<Account>>() {}
-                );
+                readJson("data/accounts.json",
+                        new TypeReference<List<Account>>() {});
 
         accountRepository.saveAll(accounts);
 
-        System.out.println("Accounts loaded successfully: "
-                + accounts.size());
+        System.out.println("Accounts loaded: " + accounts.size());
+    }
+
+    private void loadPayments() throws Exception {
+        if (paymentRepository.count() > 0) return;
+
+        List<PaymentOutcome> payments =
+                readJson("data/payment.json",
+                        new TypeReference<List<PaymentOutcome>>() {});
+
+
+        payments.forEach(p -> {
+            p.setStatus(randomStatus());
+            p.setProcessedAt(
+                    Instant.now().minusSeconds(
+                            ThreadLocalRandom.current().nextInt(100000))
+            );
+            p.setProcessingTimeMs(
+                    ThreadLocalRandom.current().nextLong(50, 1500)
+            );
+        });
+
+        paymentRepository.saveAll(payments);
+
+        System.out.println("Payments loaded: " + payments.size());
+    }
+
+    private <T> T readJson(String filePath,
+                           TypeReference<T> typeReference) throws Exception {
+
+        try (InputStream is =
+                     new ClassPathResource(filePath).getInputStream()) {
+            return objectMapper.readValue(is, typeReference);
+        }
+    }
+
+    private String randomStatus() {
+        String[] statuses = {"PROCESSED", "HELD", "REJECTED"};
+        return statuses[
+                ThreadLocalRandom.current()
+                        .nextInt(statuses.length)
+                ];
     }
 }
