de53e7ba8632a3984ae66431e343be041ba1924d
Subrata <subrata.dey88@gmail.com>
2026-05-09 14:52:57 +0530

    Commit

---
+++ src/main/java/com/lloyds/payments/aspect/LoginAspect.java
@@ -0,0 +1,30 @@
+package com.lloyds.payments.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoginAspect {
+
+    Logger logger = LoggerFactory.getLogger(LoginAspect.class);
+
+    @Pointcut("execution(* com.lloyds.payments.ingestor.*.*.*(..))")
+    public void PaymentIngestor(){
+    }
+
+    @Around("PaymentIngestor()")
+    public Object loginPaymentIngestor(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed(); // Executes the actual method
+        long executionTime = System.currentTimeMillis() - start;
+        logger.info("{} executed in {}ms{}", joinPoint.getSignature(), executionTime, proceed);
+        return proceed; // Return the actual result from the method
+    }
+
+}
